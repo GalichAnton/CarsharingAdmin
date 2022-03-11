f83a5b97152fe3ce1ecf414d7ae0a602de49d3dc
Anton Galich <inthesky133@gmail.com>
2022-03-11 14:36:04 +0300

    V-07 fix fonts

--- src/validation/LoginValidation.ts
+++ src/validation/LoginValidation.ts
@@ -1,23 +1,17 @@
 import * as yup from "yup";
-
+const regexp = /[a-zA-Z0-9]/;
 const loginValidationSchema = yup.object().shape({
   username: yup
     .string()
     .required("'Обзательное поле, минимум 3 символа'")
     .min(3, "Минимум 3 символа")
-    .matches(
-      /[a-zA-Z0-9]/,
-      "Логин должен использовать латинские буквы и цифры"
-    ),
+    .matches(regexp, "Логин должен использовать латинские буквы и цифры"),
 
   password: yup
     .string()
     .required("Минимум 4 символа")
     .min(4, "Обзательное поле, минимум 4 символа")
-    .matches(
-      /[a-zA-Z0-9]/,
-      "Пароль должен использовать латинские буквы и цифры"
-    ),
+    .matches(regexp, "Пароль должен использовать латинские буквы и цифры"),
 });
 
 export default loginValidationSchema;
